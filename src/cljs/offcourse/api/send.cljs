a69fd8d1559e7c17050cf642e8d692f9ab67f337
Jan Hein Hoogstad <yeehaa@offcourse.io>
2016-09-18 21:02:04 +0200

    refactor event spec

--- src/cljs/offcourse/api/send.cljs
+++ src/cljs/offcourse/api/send.cljs
@@ -7,9 +7,9 @@
             [shared.protocols.loggable :as log])
   (:require-macros [cljs.core.async.macros :refer [go]]))
 
-(defmulti send (fn [_ event] (sp/resolve event)))
+(defmulti send (fn [_ [event-type _]] event-type))
 
-(defmethod send :default
+(defmethod send :not-found
   [{:keys [component-name repositories] :as api} [_ query :as event]]
   (doseq [{:keys [resources] :as repository} repositories]
     (when (contains? resources (sp/resolve query))
--- src/cljs/offcourse/router/react.cljs
+++ src/cljs/offcourse/router/react.cljs
@@ -5,14 +5,14 @@
             [shared.protocols.loggable :as log]
             [shared.protocols.actionable :as ac]))
 
-(defmulti react (fn [_ [_ payload :as event]] (sp/resolve event)))
+(defmulti react (fn [_ [event-type _]] event-type))
 
-(defmethod react [:refreshed :data] [{:keys [history routes url-helpers responses]} [_ payload]]
+(defmethod react :refreshed [{:keys [history routes url-helpers responses]} [_ payload]]
   (let [{:keys [type] :as viewmodel} (-> payload :viewmodel)
         old-url (pushy/get-token history)
         new-url (cv/to-url viewmodel routes)]
     (when-not (= old-url new-url)
       (pushy/replace-token! history new-url))))
 
-(defmethod react [:requested :action] [rt [_ action]]
+(defmethod react :requested [rt [_ action]]
   (ac/perform rt action))
