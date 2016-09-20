989cdb0478c9bbdc78ffa037501afafc56a2f4c8
Zaturrby <rjkorteschiel@gmail.com>
2016-09-20 20:00:43 +0200

    fix src-prod

--- src-prod/cljs/offcourse/main.cljs
+++ src-prod/cljs/offcourse/main.cljs
@@ -3,26 +3,48 @@
             [offcourse.adapters.aws.index :as aws]
             [offcourse.adapters.github.index :as github]
             [offcourse.core :as core]
-            [shared.models.appstate.index :as model]))
-
-(set! cljs.core/*print-fn* identity)
-
-(defonce appstate (atom (model/new {:site-title "Offcourse_"})))
+            [shared.models.appstate.index :as model]
+            [shared.specs.action :as action]))
 
 (defonce app (atom nil))
 
-(defonce auth-config {:domain "yeehaa.eu.auth0.com"
-                      :clientID "Z1J0CyMzZfIbOfBSVaMWJakoIrxm4Tfs"})
+(defonce appstate-data {:site-title "Offcourse_"
+                        :actions action/action-types})
+
+(defonce appstate (atom (model/create appstate-data)))
+
+(def auth-adapter
+  {:domain   "yeehaa.eu.auth0.com"
+   :clientID "Z1J0CyMzZfIbOfBSVaMWJakoIrxm4Tfs"})
+
+(def query-adapters
+  [{:adapter    aws/create
+    :name      "query"
+    :resources #{:user :resources}
+    :endpoint  "https://f4gjbp5ur5.execute-api.us-east-1.amazonaws.com/dev/query"}
+   #_{:adapter    github/create
+    :name       "html-course"
+    :repository {:name         "html-css-javascript"
+                 :organization "offcourse"
+                 :curator      "charlotte"
+                 :sha          "4f98f9cf8d521dc3bb8c4489d91f6c9468f76c69"}
+    :resources  #{:course :collection}
+    :base-url   "https://api.github.com"}
+   {:adapter    github/create
+    :name       "bootstrap-data"
+    :repository {:name         "clojurescript-course"
+                 :organization "offcourse"
+                 :curator      "charlotte"
+                 :sha          "31510c353ec5d24a79b0b8d1a68d4d373d1f2d3f"}
+    :resources  #{:course :collection}
+    :base-url   "https://api.github.com"}])
 
 (def adapters
-  [{:adapter           github/new-db
-    :name              "bootstrap"
-    :repository        {:name "clojurescript-course"
-                        :organization      "offcourse"
-                        :curator           "charlotte"
-                        :sha               "31510c353ec5d24a79b0b8d1a68d4d373d1f2d3f"}
-    :resources         #{:course :collection}
-    :endpoint          "https://api.github.com"}])
+  {:auth auth-adapter
+   :command nil
+   :query query-adapters})
+
+(defn get-appstate [] (clj->js @appstate))
 
 (defn init []
   (do
