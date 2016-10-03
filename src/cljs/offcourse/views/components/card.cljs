42e3445b86616f7025543a2075bd9859bd10c0e4
Zaturrby <rjkorteschiel@gmail.com>
2016-10-03 17:23:52 +0200

    fix card

--- src/cljs/offcourse/views/components/card.cljs
+++ src/cljs/offcourse/views/components/card.cljs
@@ -5,32 +5,36 @@
 
 (rum/defc card [{:keys [course-id goal course-slug checkpoints curator] :as course}
                 respond]
-  (let [{:keys [affordances course-url]} (meta course)
-        {:keys [browsable? forkable? trackable? editable?]} affordances]
-   [:.container
-    [:.card
-     [:.card--section
-      [:a.card--title {:href (-> course meta :course-url)} goal]]
-     [:.card--section
-       [:.card--profile
-        [:.card--profile-section
-          [:img.card--profile-image {:src (str "/images/profilepics/" (:curator course) ".jpg")}]]
-        [:.card--profile-section
-         [:.card--profile-label "Curated by"]
-         [:.card--profile-username (clojure.string/capitalize (:curator course))]]]]
-     [:.card--section (item-list :todo checkpoints trackable? respond)]
+ (let [{:keys [affordances course-url]} (meta course)
+       {:keys [browsable? forkable? trackable? editable?]} affordances]
+  [:.container
+   [:.card
+    [:.card--section
+     [:a.card--title {:href (-> course meta :course-url)} goal]]
+    [:.card--section
+     [:.card--profile
+      [:.card--profile-section
+       [:img.card--profile-image {:src (str "/images/profilepics/" (:curator course) ".jpg")}]]
+      [:.card--profile-section
+       [:.card--profile-label "Curated by"]
+       [:.card--profile-username (clojure.string/capitalize (:curator course))]]]]
+    [:.card--section (item-list :todo checkpoints trackable? respond)]
+    (when forkable?
      [:.card--section
       [:ul.card--actions
-       #_(when browsable? (button "Browse" course-url))
-       (when forkable? (button "Fork" #(respond [:fork course])))]]
-     #_[:.card--section
+       ; (when browsable? (button "Browse" course-url))
+       (when forkable? (button "Fork" #(respond [:fork course])))]])
+    (when-let [socialable? false]
+     [:.card--section
       [:.card--social
        [:.card--social-icons
         [:img.card--social-img {:src "/images/social/t.png"}]
         [:img.card--social-img {:src "/images/social/f.png"}]
         [:img.card--social-img {:src "/images/social/gplus.png"}]
         [:img.card--social-img {:src "/images/social/gh.png"}]]
-       [:.card--social-url "Get URL"]]]]]))
+       [:.card--social-url "Get URL"]]])]]))
 
 (rum/defc cards [{:keys [courses]} respond]
   [:.cards (map #(rum/with-key (card % respond) (:course-id %)) courses)])
+
+
