8b1adabe5dbc08575ae623aa5e9c8de7dd076878
Jan Hein Hoogstad <yeehaa@offcourse.io>
2016-10-10 18:30:37 +0200

    validations and dirty check

--- src/cljs/offcourse/views/components/course_form.cljs
+++ src/cljs/offcourse/views/components/course_form.cljs
@@ -20,16 +20,18 @@
   (swap! course-atom #(co/create-checkpoint course)))
 
 (rum/defcs course-form < (rum/local {} ::course) [state {:keys [course]} respond]
- (let [course-atom (::course state)
-       course (merge course @course-atom)
-       valid? (sp/valid? course)]
+  (let [course-atom (::course state)
+        old-course  course
+        course      (merge course @course-atom)
+        dirty?      (not= course old-course)
+        valid?      (sp/valid? course)]
   [:.course-form
    [:.course-form--section {:key :title}
     [:.course-form--action-title "Edit the Title"]
-    [:input.course-form--course-title {:key         "title"
-                                       :type        :text
-                                       :value      (:goal course)
-                                       :on-change   #(update-prop :goal % course-atom)}]]
+    [:input.course-form--course-title {:key       "title"
+                                       :type      :text
+                                       :value     (:goal course)
+                                       :on-change #(update-prop :goal % course-atom)}]]
    [:.course-form--section {:key :tasks}
     [:.course-form--action-title "Edit the Resources"]
     [:.course-form--list (edit-list (:checkpoints course)
@@ -39,5 +41,5 @@
       (when true (button "Add Checkpoint" #(create-checkpoint course-atom course)))]]
    [:.course-form--section {:key :actions}
     [:.course-form--actions
-     [(when true (button "Save Course" (partial log/log "Saving Course... or not")))]
+     [(when (and valid? dirty?) (button "Save Course" (partial log/log "Saving Course... or not")))]
      [(when true (button "Cancel" #(respond [:switch-to :view-mode])))]]]]))
