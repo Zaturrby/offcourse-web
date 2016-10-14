f65669444089f7e0ad2c4e3517ad5dd9dad0cf35
Zaturrby <rjkorteschiel@gmail.com>
2016-10-14 17:53:25 +0200

    deactivate gh-key, headerlinks and responsive dropdown

--- src/cljs/offcourse/views/components/card.cljs
+++ src/cljs/offcourse/views/components/card.cljs
@@ -11,30 +11,29 @@
    [:.card
     [:.card--section
      [:a.card--title {:href (-> course meta :course-url)} goal]]
-    [:.card--section
-     [:.card--profile
-      [:.card--profile-section
-       [:img.card--profile-image {:src (str "/images/profilepics/" (:curator course) ".jpg")}]]
-      [:.card--profile-section
-       [:.card--profile-label "Curated by"]
-       [:.card--profile-username (clojure.string/capitalize (:curator course))]]]]
-    [:.card--section (item-list :todo checkpoints trackable? respond)]
-    (when forkable?
+    [:.card--dropdown
      [:.card--section
-      [:ul.card--actions
-       ; (when browsable? (button "Browse" course-url))
-       (when forkable? (button "Fork" #(respond [:fork course])))]])
-    (when-let [socialable? false]
-     [:.card--section
-      [:.card--social
-       [:.card--social-icons
-        [:img.card--social-img {:src "/images/social/t.png"}]
-        [:img.card--social-img {:src "/images/social/f.png"}]
-        [:img.card--social-img {:src "/images/social/gplus.png"}]
-        [:img.card--social-img {:src "/images/social/gh.png"}]]
-       [:.card--social-url "Get URL"]]])]]))
+      [:.card--profile
+       [:.card--profile-section
+        [:img.card--profile-image {:src (str "/images/profilepics/" (:curator course) ".jpg")}]]
+       [:.card--profile-section
+        [:.card--profile-label "Curated by"]
+        [:.card--profile-username (clojure.string/capitalize (:curator course))]]]]
+     [:.card--section (item-list :todo checkpoints trackable? respond)]
+     (when forkable?
+      [:.card--section
+       [:ul.card--actions
+        ; (when browsable? (button "Browse" course-url))
+        (when forkable? (button "Fork" #(respond [:fork course])))]])
+     (when-let [socialable? false]
+      [:.card--section
+       [:.card--social
+        [:.card--social-icons
+         [:img.card--social-img {:src "/images/social/t.png"}]
+         [:img.card--social-img {:src "/images/social/f.png"}]
+         [:img.card--social-img {:src "/images/social/gplus.png"}]
+         [:img.card--social-img {:src "/images/social/gh.png"}]]
+        [:.card--social-url "Get URL"]]])]]]))
 
 (rum/defc cards [{:keys [courses]} respond]
   [:.cards (map #(rum/with-key (card % respond) (:course-id %)) courses)])
-
-
