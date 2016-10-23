8a522e9ad0a0c18465f483ffc12df2fcaaad4a04
Zaturrby <rjkorteschiel@gmail.com>
2016-10-23 15:13:40 +0200

    add dropdown state

--- src/cljs/offcourse/views/components/course_form.cljs
+++ src/cljs/offcourse/views/components/course_form.cljs
@@ -20,15 +20,23 @@
 (defn create-checkpoint [course-atom course]
   (swap! course-atom #(co/create-checkpoint course)))
 
-(rum/defcs course-form < (rum/local {} ::course) [state {:keys [course]} respond]
-  (let [course-atom (::course state)
-        course      (merge course @course-atom)
-        errors (:cljs.spec/problems (sp/errors course))
-        error-paths (into #{} (map #(-> % :path first) errors))
-        old-course  course
-        dirty?      (not= course old-course)
-        valid?      (sp/valid? course)]
-    [:.card {:data-card-type :wide}
+(defn set-dropdown [dropdown-name dropdown-atom]
+  (swap! dropdown-atom #(str dropdown-name)))
+
+(rum/defcs course-form < (rum/local {} ::course)
+                         (rum/local "" ::dropdown)
+  [state {:keys [course]} respond]
+  (let [course-atom   (::course state)
+        course        (merge course @course-atom)
+        errors        (:cljs.spec/problems (sp/errors course))
+        error-paths   (into #{} (map #(-> % :path first) errors))
+        old-course     course
+        dirty?        (not= course old-course)
+        valid?        (sp/valid? course)
+        dropdown-atom (::dropdown state)
+        dropdown?     @dropdown-atom]
+    [:.card {:data-card-type :wide
+             :on-click (when (not= dropdown? "") #(set-dropdown "" dropdown-atom))}
       [:.card--section
         [:h1.card--title {:data-title-indent true} "Curate a course"]]
       [:.card--section
@@ -36,10 +44,12 @@
                       :data-row-padded :small}
           [:p.card--subtitle {:data-subtitle-indent true}
                              "Title of the course"]
-          [:p.card--link {:data-link-type :em} "How to write a catchy title"]
+          [:p.card--link {:data-link-type :em
+                          :on-click #(set-dropdown "title" dropdown-atom)}
+                         "How to write a catchy title"]
           (dropdown {:title "Titles"
                      :text "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
-                     :shown false})]
+                     :shown (= dropdown? "title")})]
         (when (contains? error-paths :goal) [:.card--error "This field is not correct yet"])
         [:input.form--field {:key       "title"
                              :type      :text
@@ -51,15 +61,17 @@
                       :data-row-padded :small}
           [:p.card--subtitle {:data-subtitle-indent true}
                              "Add or edit resources"]
-          [:p.card--link {:data-link-type :em} "What are resources?"]
-          (dropdown {:title "Titles"
+          [:p.card--link {:data-link-type :em
+                          :on-click #(set-dropdown "resources" dropdown-atom)}
+                         "What are resources?"]
+          (dropdown {:title "Resources"
                      :text "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
-                     :shown false})]
+                     :shown (= dropdown? "resources")})]
         (edit-list (:checkpoints course)
                    #(update-checkpoint course-atom course %1)
                    #(remove-checkpoint course-atom course %1))
         (when true
-          (button {:button-text "Add Checkpoint"}
+          (button {:button-text "+" :button-color :light :button-width :full}
                   #(create-checkpoint course-atom course)))]
      [:.card--section
        [:.card--row {:data-row-spaced true}
