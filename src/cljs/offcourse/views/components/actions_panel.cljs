d1710245f74235013bc556a5564bce8d0b80b10d
Zaturrby <rjkorteschiel@gmail.com>
2016-10-23 19:40:48 +0200

    add temporary overlay buttons to action panel to test overlay activation

--- src/cljs/offcourse/views/components/actions_panel.cljs
+++ src/cljs/offcourse/views/components/actions_panel.cljs
@@ -3,29 +3,17 @@
             [cuerdas.core :as str]
             [shared.protocols.loggable :as log]))
 
-(defn button-title [string]
-  (-> string
-      name
-      str/humanize
-      str/titleize))
-
-(defn url-button [action-name url]
-  [:li.actions-panel--link
-   {:key [action-name]}
-   [:a {:href url} (button-title action-name)]])
-
-(defn handler-button [action-name respond]
-  [:li.actions-panel--link {:key [action-name]
-                            :on-click #(respond [action-name])}
-   (button-title action-name)])
+(defn handler-button [button-text respond]
+  [:li.actions-panel--link {:on-click #(respond)}
+   button-text])
 
 (rum/defc actions-panel [{:keys [user-name auth-token] :as data}
                          respond]
   [:ul.actions-panel
-   (when user-name [:li.actions-panel--link
-                     {:key "create"}
-                     "Create Course"])
-   (when user-name (url-button user-name "/"))
+   (handler-button "auth" (partial respond [:switch-to :auth]))
+   (handler-button "new-user" (partial respond [:switch-to :new-user]))
+   (handler-button "edit-profile" (partial respond [:switch-to :edit-profile]))
+   (handler-button "view-profile" (partial respond [:switch-to :view-profile]))
    (if auth-token
-     (handler-button :sign-out respond)
-     (handler-button :sign-in respond))])
+     (handler-button "Sign Out" (partial respond [:sign-out]))
+     (handler-button "Sign In" (partial respond [:sign-in])))])
