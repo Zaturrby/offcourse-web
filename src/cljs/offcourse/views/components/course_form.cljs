b6a97f3ad12d46e9a97767c7b41a56269e5db3c4
Zaturrby <rjkorteschiel@gmail.com>
2016-10-24 07:48:02 +0200

    rename dropdown header

--- src/cljs/offcourse/views/components/course_form.cljs
+++ src/cljs/offcourse/views/components/course_form.cljs
@@ -47,7 +47,7 @@
           [:p.card--link {:data-link-type :em
                           :on-click #(set-dropdown "title" dropdown-atom)}
                          "How to write a catchy title"]
-          (dropdown {:title "Titles"
+          (dropdown {:title "Catchy Titles"
                      :text "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
                      :shown (= dropdown? "title")})]
         (when (contains? error-paths :goal) [:.card--error "This field is not correct yet"])
