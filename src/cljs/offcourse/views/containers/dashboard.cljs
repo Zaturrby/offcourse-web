045392ab196fb233ee1a3d79d2caae6d5ea6b475
Zaturrby <rjkorteschiel@gmail.com>
2016-10-25 12:32:33 +0200

    hide access to overlays

--- src/cljs/offcourse/views/containers/dashboard.cljs
+++ src/cljs/offcourse/views/containers/dashboard.cljs
@@ -3,5 +3,5 @@
 
 (rum/defc dashboard [{:keys [main controls]}]
   [:.dashboard
-   [:.dashboard--main main]
-   [:.dashboard--controls controls]])
+   [:.dashboard--main main]])
+  ;  [:.dashboard--controls controls]])
